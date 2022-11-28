4e3d343de67d8564ce100c13d02167074b3be0c5
Thositha Soysa <ksrsoysa@gmail.com>
2022-11-27 21:32:21 -0600

    made change for submission

--- js/requestUnicorn.js
+++ js/requestUnicorn.js
@@ -10,27 +10,27 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 const fleet = [
 	{
-		Name: 'Bucephalus',
+		Name: 'Thositha Soysa 1',
 		Color: 'Golden',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Scrappy',
+		Name: 'Thositha Soysa 2',
 		Color: 'Mean Green',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Cosmo',
+		Name: 'Thositha Soysa 3',
 		Color: 'Royal Blue',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Shadowfax',
+		Name: 'Thositha Soysa 4',
 		Color: 'White',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Rocinante',
+		Name: 'Thositha Soysa 5',
 		Color: 'Yellow',
 		Gender: 'Female',
 	},
